517ec40eb0e3322d8839988308d8895052acbd21
charlie26701 <xiangtw26701@gmail.com>
2023-11-30 11:56:04 +0800

    add dotnet images

---
+++ dotnet/entrypoint.sh
@@ -0,0 +1,12 @@
+TZ = ${TZ:-"Asia/Taipei"}
+export TZ
+
+cd /home/container || exit 1
+
+printf "\033[1m\033[33mcontainer@dmitserver~ \033[0mdotnet -version\n"
+dotnet -version
+
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+echo -e "\033[1m\033[33mcontainer@dmitserver~ \033[0m${PARSED}\n"
+
+exec env ${PARSED}
