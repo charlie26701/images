c85322c91f345a889cc5d2ef97270820b406cbc6
charlie26701 <xiangtw26701@gmail.com>
2023-12-18 03:44:21 +0800

    remove dotnet --version

--- dotnet/entrypoint.sh
+++ dotnet/entrypoint.sh
@@ -3,8 +3,8 @@
 
 cd /home/container || exit 1
 
-printf "\033[1m\033[33mcontainer@dmitserver~ \033[0mdotnet --version\n"
-dotnet --version
+# printf "\033[1m\033[33mcontainer@dmitserver~ \033[0mdotnet --version\n"
+# dotnet --version
 
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 printf "\033[1m\033[33mcontainer@dmitserver~ \033[0m%s\n" "$PARSED"
