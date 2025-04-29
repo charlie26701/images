93537780ae73e6de78d1793221441e021686fbba
charlie26701 <xiangtw26701@gmail.com>
2025-04-29 10:52:28 +0800

    fix fix fix

--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -10,8 +10,4 @@ cd /home/container || exit 1
 printf "\033[1m\033[33mcontainer@ubuntu~ \033[0m"
 lsb_release -d | cut -f2
 
-# 處理啟動命令
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-printf "\033[1m\033[33mcontainer@ubuntu~ \033[0m%s\n" "$PARSED"
-
-exec env ${PARSED} 
+exec /bin/bash 
